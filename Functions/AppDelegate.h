949e04ac1bb22a6a810404af320a67d1e36be413
136357477 <guohui136357477@126.com>
2015-04-08 14:52:51 +0800

    常用的一些自己写的函数

---
+++ Functions/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Functions
+//
+//  Created by guohui on 15/4/8.
+//  Copyright (c) 2015年 guohui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Functions/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Functions
+//
+//  Created by guohui on 15/4/8.
+//  Copyright (c) 2015年 guohui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
